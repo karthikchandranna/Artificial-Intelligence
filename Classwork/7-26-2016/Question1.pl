df71e40665a8f63f243b4a319be2d4f10a79cd6e
karthikchandranna <karthik@ccs.neu.edu>
2016-07-26 17:20:33 -0400

    q1 fix

--- Classwork/7-26-2016/Question1.pl
+++ Classwork/7-26-2016/Question1.pl
@@ -69,7 +69,10 @@ isFoot(X,Y):- footpath(X,Y);footpath(Y,X).
 direct(X,Y):- isFoot(X,Y);isMotor(X,Y).
 route(X,Y):- direct(X,Y).
 route(X,Y):- direct(X,Z),route(Z,Y).
-
+motorRoute(X,Y):- direct(X,Y).
+motorRoute(X,Y):- direct(X,Z),motorRoute(Z,Y).
+footpathRoute(X,Y):- direct(X,Y).
+footpathRoute(X,Y):- direct(X,Z),footpathRoute(Z,Y).
 /**
  *	1.b
  * route(56,60)
@@ -77,20 +80,15 @@ route(X,Y):- direct(X,Z),route(Z,Y).
  * route(52,58)
  * route(58,68)
  * route(53,58)
- 
+ * footpathRoute(56,59)
+ * 
  * 1.c
  * is there a route from hall 56 to hall 60 -> route(56,60)
  * is there a route from hall 60 to hall 56 -> route(60,56)
  * is there a route from hall 52 to hall 58 -> route(52,58)
- * is there a footpath from hall 52 to hall 62 -> footpath(52,62)
+ * is there a footpath route from hall 56 to hall 59 -> footpathRoute(56,59)
  * is there a motorway from hall 53 to hall 60 -> motorway(53,60)
  * is there a footpath from hall 52 to hall 62 -> footpath(53,57)
- * */
-
-
-
-
-
-
+ */
 
 
